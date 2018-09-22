3b07396d7c0c30dd5cb23635d7512bf79380644e
Benjamin <bengohyp@gmail.com>
2018-09-23 00:04:06 +0800

    Completed tfidf

--- 00-comments-prep.py
+++ 00-comments-prep.py
@@ -57,7 +57,7 @@ def sanitize_dict(input_dict):
             pass
         else:
             # convert byte to unicode
-            if(type(input_dict[str(key)]) == bytes):
+            if(isinstance(input_dict[str(key)], type(bytes))):
                 input_dict[str(key)] = input_dict[str(key)].decode('utf-8')
             # remove non-ascii characters from strings
             input_dict[str(key)] = remove_non_ascii(input_dict[str(key)])
@@ -77,7 +77,7 @@ def sanitize_dict(input_dict):
     return input_dict
 
 
-def generate_wordcloud(input_dict, output_file_path):
+def generate_wordcloud(input_dict, custom_stopwords, output_file_path):
     import nltk
     import string
     from nltk import word_tokenize, FreqDist
@@ -93,6 +93,7 @@ def generate_wordcloud(input_dict, output_file_path):
     # Create a stopword list from the standard list of stopwords available in
     # nltk
     stop = stopwords.words('english')
+    stop.extend(custom_stopwords)
     # Remove all these stopwords from the text
     tokens_nostop = [t for t in tokens_lower if t not in stop]
     # Use snowball stemmer
@@ -102,19 +103,28 @@ def generate_wordcloud(input_dict, output_file_path):
     wnl = nltk.WordNetLemmatizer()
     tokens_lem = [wnl.lemmatize(t) for t in tokens_nostop]
     # Further cleaning: filter off anything with less than 3 characters
-    tokens_clean = [t for t in tokens_snow if len(t) >= 3]
+    tokens_clean = [t for t in tokens_lem if len(t) >= 3]
     fd_clean = nltk.FreqDist(tokens_clean)
-    text_clean = " ".join(tokens_clean)
+    # Join the cleaned tokens back into a string
+    text_clean = [''.join(f) for f in tokens_clean]
     # Generate simple wordcloud
     import wordcloud
     from wordcloud import WordCloud
     import matplotlib.pyplot as plt
-    wc = WordCloud(background_color="white").generate(text_clean)
-    # plt.imshow(wc, interpolation='bilinear')
-    # plt.axis("off")
-    # plt.show()
+    wc = WordCloud(background_color="white").generate_from_frequencies(
+        fd_clean)
     # Save wordcloud to file
     wc.to_file(output_file_path)
+    return text_clean
+
+
+def generate_tfidf(input_list):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    # Create tfidf indexing
+    vec_tfidf = TfidfVectorizer(min_df=2)
+    output_tfidf = vec_tfidf.fit_transform(input_list)
+    # output_tfidf.toarray()[:10]
+    return output_tfidf
 
 
 FILEPATH = 'data/comments.json'
@@ -122,11 +132,17 @@ KEY1 = 'title'
 KEY2 = 'text'
 comments_title = {}
 comments_title = min_extract_to_dict(FILEPATH, KEY1)
-comments_text = {}
-comments_text = min_extract_to_dict(FILEPATH, KEY2)
+# comments_text = {}
+# comments_text = min_extract_to_dict(FILEPATH, KEY2)
 comments_title_sanitized = {}
 comments_title_sanitized = sanitize_dict(comments_title)
-comments_text_sanitized = {}
-comments_text_sanitized = sanitize_dict(comments_text)
-generate_wordcloud(comments_title_sanitized, 'results/comments_title.png')
-generate_wordcloud(comments_text_sanitized, 'results/comments_text.png')
+# comments_text_sanitized = {}
+# comments_text_sanitized = sanitize_dict(comments_text)
+custom_stopwords_title = []
+comments_title_clean = generate_wordcloud(comments_title_sanitized,
+                                          custom_stopwords_title,
+                                          'results/comments_title_lem.png')
+# custom_stopwords_text = []
+# comments_text_clean = generate_wordcloud(comments_text_sanitized,
+#                                          custom_stopwords_text,
+#                                          'results/comments_text_lem.png')
