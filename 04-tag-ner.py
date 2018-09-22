f3716b801983da1fdb4e759189511189ecf7102a
Benjamin <bengohyp@gmail.com>
2018-09-22 15:57:31 +0800

    Upload day 4 exercises

--- 04-tag-ner.py
+++ 04-tag-ner.py
@@ -10,9 +10,9 @@ from nltk import word_tokenize, pos_tag, ne_chunk
 
 # ===== POS Tagging and NER using NLTK =====
 
-sent = '''Professor Tan Eng Chye, NUS Deputy President and Provost, and Professor 
-Menahem Ben-Sasson, President of HUJ signed the joint degree agreement at NUS, 
-in the presence of Ambassador of Israel to Singapore Her Excellency Amira Arnon 
+sent = '''Professor Tan Eng Chye, NUS Deputy President and Provost, and Professor
+Menahem Ben-Sasson, President of HUJ signed the joint degree agreement at NUS,
+in the presence of Ambassador of Israel to Singapore Her Excellency Amira Arnon
 and about 30 invited guests, on July 03, 2013.
 '''
 
@@ -26,20 +26,20 @@ sent_chunk = ne_chunk(sent_pos)
 print(sent_chunk)
 
 # ===== Now try creating your own named entity and noun phrase chunker ====
-# We need to define the tag patterns to capture the target phrases and use 
+# We need to define the tag patterns to capture the target phrases and use
 # RegexParser to chunk the input with those patterns.
-# Some minimal tag patterns are given here. 
+# Some minimal tag patterns are given here.
 
 grammar = r"""
-  NE: {<NNP>}      # chunk sequences of proper nouns
-  NP:                 
-      {<DT><NN>}     
+  NE: {<NNP><IN><NNP>}
+      {<NNP>*} # chunk sequences of proper nouns
+  NP: {<DT>?<NN>*}
 """
 
 cp = nltk.RegexpParser(grammar)
 print(cp.parse(sent_pos))
 
-#------------------------------------------------------------------------
-# Exercise: modify the above tag patterns to capture the NEs and NPs in the 
-# example sentence. 
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# Exercise: modify the above tag patterns to capture the NEs and NPs in the
+# example sentence.
+# -------------------------------------------------------------------------
